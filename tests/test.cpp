9348378bdf8e028c0827ec4f34b1cb275414ec13
kam1runetz <notsoserious2017@gmail.com>
2021-01-21 20:35:17 +0300

    some coment

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,6 +2,44 @@
 
 #include <gtest/gtest.h>
 
+#include <sstream>
+#include <string>
+#include <students_from_json.hpp>
+
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+  std::string json = R"({
+    "items": [
+    {
+      "name": "Ivanov Petr",
+          "group": "1",
+          "avg": "4.25",
+          "debt": null
+    },
+    {
+      "name": "Sidorov Ivan",
+          "group": 31,
+          "avg": 4,
+          "debt": "C++"
+    },
+    {
+      "name": "Pertov Nikita",
+          "group": "IU8-31",
+          "avg": 3.33,
+          "debt": [
+      "C++",
+          "Linux",
+          "Network"
+      ]
+    }
+    ],
+    "_meta": {
+      "count": 3
+    }
+  })";
+  std::stringstream jsonStream;
+  jsonStream << json;
+
+  std::vector<Student> students = ParseJSON(jsonStream);
+  Print(students, std::cout);
+  EXPECT_TRUE(true);
 }
